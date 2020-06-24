0e6bff49004606caea946ba48e5ef1c1fea99697
revathi.m73@wipro.com <revathi.m73@wipro.com>
2020-06-24 11:21:04 +0530

    files added

---
+++ src/main/java/com/wipro/spring/Controller/Springcontroller.java
@@ -0,0 +1,52 @@
+package com.wipro.spring.Controller;
+
+import java.awt.PageAttributes.MediaType;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.wipro.spring.Bean.Bean;
+
+@Controller
+public class Springcontroller {
+	@Autowired(required=true)
+	Bean b;
+	@RequestMapping("/")
+	public String employee() {
+		return "index.html";
+	}
+	
+	@RequestMapping(path="/check")
+	public String currency(@RequestParam(required = true) String num,
+            @RequestParam(required = true) String Currency,Model model) {
+		
+		
+	int n=Integer.parseInt(num);
+	String Curr=Currency.toString();
+	//Bean b=new Bean();
+		float res=0;
+		if(Curr.equalsIgnoreCase("Dollar"))
+		{
+			res=71*n;
+			
+		}else if(Curr.equalsIgnoreCase("Euro"))
+		{
+			res=78*n;
+			
+		}else if(Curr.equalsIgnoreCase("Riyal"))
+		{
+			res=18*n;
+		}
+	
+	//System.out.print(b.getRes());
+	
+	model.addAttribute("cur", res);
+		
+		return "index1.html";
+	}
+
+}
