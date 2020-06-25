6bcb9cf50ae06daab0022a5b55cf4230e6d6e63d
REVATHISARAISWATHI <revathisaraiswathi@gmail.com>
2020-06-25 13:39:47 +0530

    Update SpringAppApplication.java

--- src/main/java/com/wipro/spring/SpringAppApplication.java
+++ src/main/java/com/wipro/spring/SpringAppApplication.java
@@ -2,12 +2,16 @@ package com.wipro.spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
-public class SpringAppApplication {
+public class SpringAppApplication extends ServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringAppApplication.class, args);
 	}
-
+	@Override
+	  protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+	      return builder.sources(SpringAppApplication.class);
+	  }
 }
