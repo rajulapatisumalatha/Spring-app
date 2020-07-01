827f7b2d939af5e8cc8faf3f0952b3e40044f317
REVATHISARAISWATHI <revathisaraiswathi@gmail.com>
2020-07-01 14:52:15 +0530

    Update SpringAppApplicationTests.java

--- src/test/java/com/wipro/spring/SpringAppApplicationTests.java
+++ src/test/java/com/wipro/spring/SpringAppApplicationTests.java
@@ -1,7 +1,13 @@
 package com.wipro.spring;
 
+
+import static org.junit.Assert.assertEquals;
+
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.wipro.spring.dao.bean;
 
 @SpringBootTest
 class SpringAppApplicationTests {
@@ -9,5 +15,26 @@ class SpringAppApplicationTests {
 	@Test
 	void contextLoads() {
 	}
+	@Test
+	    public void doller() throws Exception {
+		 bean b=new bean("Dollar",2);
+			b.curr();
+			int res=b.getRes();
+			assertEquals(142,res);
+	    }
+	 @Test
+	    public void ero() throws Exception {
+		 bean bt1=new bean("Euro",2);
+			bt1.curr();
+			int res1=bt1.getRes();
+			assertEquals(156,res1);
+	    }
+	 public void riyal() throws Exception {
+		 bean bt2=new bean("Riyal",2);
+			bt2.curr();
+			int res2=bt2.getRes();
+			assertEquals(36,res2);
+	    }
+	 
 
 }
